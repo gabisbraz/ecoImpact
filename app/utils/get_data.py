65466607c7984993f7b9a3c65b7309b4930ad68d
Gabriella Braz <gabibraz15@outlook.com>
2024-11-09 20:14:17 -0300

    [ADD] PATHS TO IMAGE

--- app/utils/get_data.py
+++ app/utils/get_data.py
@@ -1,10 +1,17 @@
+import sys
+from pathlib import Path
 import streamlit as st
 import pandas as pd
 
 
+DIR_ROOT = str(Path(__file__).parents[1])
+if DIR_ROOT not in sys.path:
+    sys.path.append(DIR_ROOT)
+
+
 @st.cache_data(show_spinner=False)
 def get_data():
-    df = pd.read_excel("data/output/BASE_FINAL.xlsx")
+    df = pd.read_excel(Path(DIR_ROOT, "data/output/BASE_FINAL.xlsx"))
     df.columns = [
         "Marca",
         "Modelo",
